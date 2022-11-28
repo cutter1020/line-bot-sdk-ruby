3d90e8e57fedb517fcca755fcc5670b60949c0e4
cutter1020 <58689319+cutter1020@users.noreply.github.com>
2022-11-28 16:34:09 +0700

    Update app.rb

--- examples/echobot/app.rb
+++ examples/echobot/app.rb
@@ -32,7 +32,7 @@ post '/callback' do
         client.reply_message(event['replyToken'], message)
         # Publish example
         MQTT::Client.connect('broker.emqx.io') do |c|
-          c.publish('testtest', 'message')
+          c.publish('testtest', message)
         end
       end
     end
