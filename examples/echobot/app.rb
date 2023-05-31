d7dc4c92ad48d4d7f85966e487bb3d430ba5acfd
cutter1020 <58689319+cutter1020@users.noreply.github.com>
2023-05-31 16:14:59 +0700

    test36

--- examples/echobot/app.rb
+++ examples/echobot/app.rb
@@ -26,6 +26,7 @@ post '/callback' do
     events = client.parse_events_from(body)
     events.each do |event|
       case event
+      when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
